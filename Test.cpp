4ab2c02e2057e8ce2f5da587e16d3cad4ae38097
Raafat marzuq <raafatmar@gmail.com>
2020-03-22 16:03:45 +0200

    75+% of the tests

--- Test.cpp
+++ Test.cpp
@@ -22,7 +22,7 @@ TEST_CASE("Test replacement of lower-case and upper-case") {
     CHECK(find(text, "HaPpI") == string("Happi"));
     /* Add more checks here */
 }
-TEST_CASE("Test replacement of w and v") {
+TEST_CASE("Test replacement of w and v , also of lower-case and upper-case") {
     string text = "I bought a new volkswagen";
     CHECK(find(text, "volksvagen") == string("volkswagen"));
     CHECK(find(text, "wolkswagen") == string("volkswagen"));
@@ -38,7 +38,7 @@ TEST_CASE("Test replacement of w and v") {
 
 }
 
-TEST_CASE("Test replacement of i and y") {
+TEST_CASE("Test replacement of i and y ,also of  lower-case and upper-case") {
     string text = "I know how to spill superkalifragilistikexpialigetisch";
     CHECK(find(text, "superkalyfragilystikexpyaligetisch") == string("superkalifragilistikexpialigetisch"));
     CHECK(find(text, "SuperkalYfragIlistIkexpYaligetisch") == string("superkalifragilistikexpialigetisch"));
@@ -54,7 +54,7 @@ TEST_CASE("Test replacement of i and y") {
     CHECK(find(text, "superkalifRAGYlistikEXpialigetisch") == string("superkalifragilistikexpialigetisch"));
    
 }
-TEST_CASE("Test replacement of j and g") {
+TEST_CASE("Test replacement of j and g,also of  lower-case and upper-case") {
     string text = " Sir Roger George Moore was an English actor best known for playing British secret agent James Bond";
     CHECK(find(text, "Rojer") == string("Roger"));
     CHECK(find(text, "georje") == string("George"));
@@ -69,7 +69,7 @@ TEST_CASE("Test replacement of j and g") {
 
 } 
 
-TEST_CASE("Test replacement of more than two letters") {
+TEST_CASE("Test replacement of more than two letters,also of  lower-case and upper-case") {
     string text = "Roll the dice twice";
     CHECK(find(text, "tike") == string("dice"));
     CHECK(find(text, "rull") == string("Roll"));
@@ -83,4 +83,33 @@ TEST_CASE("Test replacement of more than two letters") {
     CHECK(find(text, "TvICE") == string("twice"));
     CHECK(find(text, "THE") == string("the"));
 
+}
+TEST_CASE("Test replacement ofmore than two letters,also lower-case and upper-case") {
+    string text = "the suspect was eating pizza";
+    CHECK(find(text, "DHE") == string("the"));
+    CHECK(find(text, "SuSbEcT") == string("suspect"));
+    CHECK(find(text, "zozbekt") == string("suspect"));
+    CHECK(find(text, "SUSPECT") == string("suspect"));
+    CHECK(find(text, "Vaz") == string("was"));
+    CHECK(find(text, "wAZ") == string("was"));
+    CHECK(find(text, "eading") == string("eating"));
+    CHECK(find(text, "eatynj") == string("eating"));
+    CHECK(find(text, "EATING") == string("eating")); 
+    CHECK(find(text, "BiZza") == string("pizza"));
+    CHECK(find(text, "bissa") == string("pizza"));
+    CHECK(find(text, "PyzZa") == string("pizza")); 
+    CHECK(find(text, "PIZZA") == string("pizza"));                      
+}
+TEST_CASE("Test replacement ofmore than two letters,also lower-case and upper-case") {
+    string text = "The eagle has left the building";
+    CHECK(find(text, "the") == string("The"));
+    CHECK(find(text, "eajle") == string("eagle"));
+    CHECK(find(text, "HAs") == string("has"));
+    CHECK(find(text, "Haz") == string("has"));    
+    CHECK(find(text, "lept") == string("left"));
+    CHECK(find(text, "LeFt") == string("left"));
+    CHECK(find(text, "boilding") == string("building"));
+    CHECK(find(text, "puildyng") == string("building"));
+    CHECK(find(text, "BUILding") == string("building"));
+    CHECK(find(text, "BUILDING") == string("building"));
 }
